4a378fd67464c459467c836d27ac37c837d58b99
PlashkinRoman <plashkinstudy@gmail.com>
2015-10-13 15:39:58 +0500

    Плашкин Роман (правка №1)

--- roman-time.js
+++ roman-time.js
@@ -1,25 +1,26 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-if ((!(Number.isInteger(hours))) || (hours < 0) || (hours > 23) || (!(Number.isInteger(minutes))) || (minutes < 0) || (minutes > 59)) {
-  console.log('Время указано не верно');
+var arbNumbers = [1, 4, 5, 9, 10, 40, 50];
+var romanNumbers = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
+
+if ((!(hours == parseInt(hours))) || (hours < 0) || (hours > 23) || (!(minutes == parseInt(minutes))) || (minutes < 0) || (minutes > 59)) {
+  console.log('Wrong time');
   }
 else {
-  console.log(arbToRoman(hours)+' : '+arbToRoman(minutes))
+  console.log(arbToRoman(hours)+' : '+arbToRoman(minutes));
   }
 
 function arbToRoman(numb) {
   if (numb == 0) return '--';
   else {
-    var arbNumbs = [1, 4, 5, 9, 10, 40, 50];
-    var romanNumbers = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
     var romanNumb = '';
-    var n = arabicNumbers.length - 1;
+    var n = arbNumbers.length - 1;
  
     while (numb > 0) {
-      if (numb > arbNumbs[n]) {
+      if (numb >= arbNumbers[n]) {
       romanNumb = romanNumb + romanNumbers[n];
-      numb = numb - arbNumbers[n]
+      numb = numb - arbNumbers[n];
       }
       else n--;
     }
